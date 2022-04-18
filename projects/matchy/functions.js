8c1cc0d35e63e42342852a732136ea2009e01443
iamdestinos <Iamdestino@yahoo.com>
2022-04-18 21:00:33 +0000

    updated matchy

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,19 +12,70 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//declare function search
+/*
+I: array of objects, string value
+O: object of search value OR null
+*/
+function search(array, string) {
+    //declare output var
+    var output;
+    //iterate through array
+    for (let i = 0; i < array.length; i++) {
+        //determine if string input and name property of selected object are the same
+        if (string == array[i].name) {
+            //if true output is equal to current place in array
+            output = array[i];
+        }
+    }
+    //determine if output has a value
+    if (output !== undefined) {
+        //if output has value return output
+        return output;
+    } else {
+        //if output has no value return null
+        return null;
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//declare function replace
+/*
+I: array of objects, string, object
+O: N/A
+*/
+function replace(animals, name, replacement) {
+    //iterate through array animals
+    for (let i = 0; i < animals.length; i++) {
+        //determine if input name is same as name property of current object
+        if (name == animals[i].name) {
+            //if true reassign current value of animals to replacement
+            animals[i] = replacement;
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//declare function remove
+/*
+I: array of objects, string
+O: N/A
+*/
+function remove(animals, name) {
+    //iterate through array animals
+    for (let i = 0; i < animals.length; i++) {
+        //determine if string name is same as name property of current object
+        if (name == animals[i].name) {
+            //if true, remove current value of animals
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////

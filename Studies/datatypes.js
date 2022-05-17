4dbf07777df6e0423f930f22d9a1e8e80263a2a4
iamdestinos <Iamdestino@yahoo.com>
2022-05-17 20:12:37 +0000

    updated studies

---
+++ Studies/datatypes.js
@@ -0,0 +1,128 @@
+/**
+* DATA TYPES:
+* 
+* 0. When storing information in a variable, how it is
+* stored is determined by the data type. The most commonly
+* used data types are numbers, strings, booleans,
+* arrays, and objects.
+* 
+* 1. Numbers are a data type which store a numerical value.
+* 
+* 2. Strings are a series of characters contained in quotes.
+* 
+* 3. Booleans are either true or false.
+* 
+* 4. Arrays are an ordered list of various values.
+* 
+* 5. Objects contain values in an unordered series of
+* key-value pairs.
+* 
+* 6. Functions are code blocks that perform a particular
+* series of actions.
+* 
+* 7. When a variable lacks a value, the variable and
+* its type is undefined.
+* 
+* 8. Null is similar to undefined, but an intentional
+* absence of a value.
+* 
+* 9. NaN, or Not-a-Number, is a value for when a number
+* data type is trying to store a value that isn't a
+* numerical value.
+* 
+* 10. Infinity is when a numerical value exceed the
+* upper or lower limit for a number.
+* 
+* 11. Primitive data types/Complex data types
+* 
+* 12. BY COPY/BY REFERENCE
+*/
+
+// 1. Numbers are a data value that store a numerical data type
+var num = 2; //usually a nunber is an integer value
+var decimal = 0.91; //can be a decimal value
+var exp = 1.2987E+10; //can be anumber with an exponential value
+
+// 2. Strings are a data value made up of a series of characters
+var string = 'This is a string'; //can use single quotes
+var string2 = "This is also a string"; //or double quotes
+
+// 3. Booleans are either true or false
+var boolean = true;
+var boolean2 = false;
+
+// 4. Arrays are an ordered list of various values
+var array = [1, 'arrays can store any kind of value', false, []];
+// Arrays are accessed by index values starting from 0
+console.log(array[0]); //prints first value in array => 1
+console.log(array[2]); //prints third value in array => false
+
+// 5. Objects store values in an unordered list of
+// key-value pairs
+var obj = {prop1: 1, prop2: 'objects can store any kind of value', prop3: true, prop4: []};
+//values are accessed by either bracket or dot notation
+console.log(obj.prop1); //prints value at key prop1 => 1
+console.log(obj['prop3']); //prints value at key prop3 => true
+
+// 6. Functions are blocks of code that perform a series of actions
+function test(){
+  console.log('Whenever test is called this happens');
+}
+test(); //will print the message in function block
+
+// 7. When a variable lacks a value, it is undefined
+var example;
+console.log(example); //prints value of example => undefined
+//a value can also be reassigned as undefined
+var example2 = 1;
+example2 = undefined;
+console.log(example2); //prints => undefined
+
+// 8. Null is an intentional absence of value
+var nullEx = null;
+console.log(nullEx); //prints => null
+
+// 9. NaN, or Not-a-Number, is when a non-numerical
+//value is assigned or used in a numerical type
+var value1 = 10;
+var value2 = 'string';
+console.log(value1 / value2); //causes error because value2 is NaN
+
+// 10. Infinity is when a numerical values exceeds the
+//upper limit for what can be stored
+var num = 1.797693134862315E+308;
+var inf = num * 1.001;
+//and -infinity is a numerical value that exceeds the lower limit for what can be stored
+var negInf = num * -1.001;
+
+// 11. Primitive (simple) data types are data types
+// that aren't objects and have no methods.
+//They are:
+var num = 10; // numbers
+var string ='strings'; //strings
+var boolean = true; //booleans
+var und = undefined; //undefined
+var nul = null; //null
+
+// 12. Complex data types are large data types
+// capable of storing an ever increasing amount of values
+//the complex data types are:
+var array = [num, string, boolean, und, null]; //arrays
+var obj = {number: num, string: string, bool: boolean}; //objects
+
+// 13. When copying a value in a variable, there are
+// two different ways the value is copied. When copying
+// a simple data type, it is copy by value, where a
+// distinct copy is made
+var example1 = 101;
+var copy1 = example1;
+example1 += 10;
+console.log(example1); //prints => 111
+console.log(copy1); //prints => 101
+//When copying a complex data type however, the copy
+// is a copy by reference, which are sharing the same link
+var example2 = [1, 2, 3];
+var copy2 = example2;
+example2[2] = 0;
+console.log(example2); //prints => [1, 2, 0]
+console.log(copy2); //prints => [1, 2, 0]
--- Studies/variables.js
+++ Studies/variables.js
@@ -1,13 +1,108 @@
 /**
+* VARIABLES:
+*
+* 0. To hold things in memory during the life cycle
+* of a program, we can use variables. Variables are
+* named identifiers that can point to values of a
+* particular type, like a number, string, boolean,
+* array, object, or another data-type. Variables are
+* called so because once created, we can change the 
+* value (and type of value) to which they point
+*
+* 1. To create a variable we use the keyword var 
+* (or let or const), followed by a name (id or alias)
+* for our variable.
+*
+* 2. There are two phases of using variables:
+* declaration and initialization (assignment)
+*
+* 3. There are three variable declaration phrases:
+* var, let, and const, that are influenced differently
+* by scope.
+*
+* 4. Hoisting is javascript's behavior of moving
+* declarations to the top of the scope.
+*/
 
- Variables
+// 1. Declaration
+// At the declaration phase, the variable myName is
+// undefined because we have not initialized it to anything
+var myName;
+console.log(myName); //prints => undefined
 
- 0. intro
+// 2. Initialization or Assignment
+// When you assign a value to an undeclared variable
+myName = 'john';
+console.log(myName); //prints => john
 
- 1. declaration and assignment
+// 3. Re-assignment
+// Changing or updating the value of a previously
+//assigned variable
+myName = 'bob';
+console.log(myName); //prints => bob
 
- 2. var, let, const
+// 4. Var keyword
+// The var keyword is the basic variable keyword
+// and is distinguished for always having a global
+// scope.
+if(true){
+  var a = 'variables with var keyword are global';
+}
+console.log(a); //prints => variables with var keyword are global
+// Variables declared with var can also be redeclared
+// without issue
+var b = 'first declaration';
+var b = 'second declaration';
+console.log(b); //prints => second declaration
+// If a variable is declared without a keyword, it is
+// treated like a variable using the var keyword.
+if(true){
+  c = 'c has no keyword';
+}
+console.log(c); // prints => c has no keyword
 
- 3. hoisting
+// 5. Let and Const keywords
+// Let and const are variable keywords that have two
+// prominent features different from keyword var.
+// Firstly, let and const make the variable block scoped
+if(true){
+  let example = 'This variable is block scoped';
+  const example2 = 'so is this one';
+  console.log(example); // prints => This variable is block scoped
+}
+console.log(example2); //will cause error example2 does not exist
+// Secondly, a variable declared with let or const
+// cannot be redeclared like a variable using var.
+let d = 1;
+let d = true; //will cause an error
+// The const keyword also has a unique feature where
+// it cannot be reassigned at all
+const ex = 'This variable cannot be reassigned';
+ex = 'This will fail'; //will cause an error, const ex cannot be reassigned
 
- */
+// 6. Hoisting
+// Hoisting is a behavior in javascript where all
+// declarations are moved to the top of their scope.
+// So in an example such as this:
+var a = true;
+if(a === true){
+  var b = false;
+  let c = 4;
+  console.log(a);
+}
+function example(input){
+  console.log(input);
+}
+// javascript will treat it as if it like this:
+function example(input){ //functions are hoisted to the top of the code
+  console.log(input);
+}
+var a; // followed by all var declarations
+var b;
+a = true;
+if(a === true){
+  let c; //let and const will only get hoisted to the top of their scope
+  b = false;
+  c = 4;
+  console.log(a);
+}
